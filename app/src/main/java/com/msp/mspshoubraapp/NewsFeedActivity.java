5da7620aa5f5cb62e56bb5cbd3cab27d0f0711b5
massoudsalem <massoudsalem@users.noreply.github.com>
2018-08-08 21:30:34 +0200

    Adding map api to the project

--- app/src/main/java/com/msp/mspshoubraapp/NewsFeedActivity.java
+++ app/src/main/java/com/msp/mspshoubraapp/NewsFeedActivity.java
@@ -1,5 +1,6 @@
 package com.msp.mspshoubraapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -97,5 +98,14 @@ public class NewsFeedActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+//    public void map(View view) {
+//
+//    }
+
+    public void map(MenuItem item) {
+        Intent map = new Intent(this,MapsActivity.class);
+        startActivity(map);
     }
 }
